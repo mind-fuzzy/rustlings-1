d117e0943be4f4ca77683f83ad5d82cc7e91bd02
mind-fuzzy <mukilesh.sethu@gmail.com>
2023-02-28 02:19:59 -0800

    Solutions to some of the exercies

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,11 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Quit,
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8)
 }
 
 struct Point {
@@ -39,6 +40,20 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::Move(x) => {
+                self.move_position(x);
+            },
+            Message::Echo(y) => {
+                self.echo(y);
+            },
+            Message::ChangeColor(a, b, c) => {
+                self.change_color((a, b, c));
+            },
+            Message::Quit => {
+                self.quit();
+            }
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,7 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -37,6 +36,17 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        let fruitValue = match fruit {
+            Fruit::Apple => 4,
+            Fruit::Banana => 1,
+            Fruit::Mango => 2,
+            Fruit::Lychee => 5,
+            Fruit::Pineapple => 1,
+        };
+
+        if !basket.contains_key(&fruit) {
+            basket.insert(fruit, fruitValue);
+        }
     }
 }
 
